ffd4a9670f6d02b62a6cee9cb3c8c61918e72a0b
Jesus Saad <jesus.saad1@gmail.com>
2021-10-03 01:57:24 -0300

    melhorando o nome do script

---
+++ shell/03-10-2021--gleiciene.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# ./insertOcurrence.sh '03/05' 'PET 7 é referente aos dias 03/05 a 07/05 e equivale a 02 aulas de Educação Física'
+# ./insertOcurrence.sh '10/05' 'PET 8 é referente aos dias 10/05 a 14/05 e equivale a 02 aulas de Educação Física'
+# ./insertOcurrence.sh '17/05' 'O PET 9 é referente aos dias 17/05 a 21/05 e equivale a 02 aulas de Educação Física'
+# ./insertOcurrence.sh '24/05' 'O PET 10 é referente aos dias 24/05 a 28/05 e equivale a 02 aulas de Educação Física'
+# ./insertOcurrence.sh '31/05' 'O PET 11 é referente aos dias 31/05 a 04/06 e equivale a 02 aulas de Educação Física'
+# ./insertOcurrence.sh '07/06' 'O PET 12 é referente aos dias 07/06 a 11/06 e equivale a 02 aulas de Educação Física'
+./insertOcurrence.sh '14/06' 'A partir de 14/06 de 2021, as aulas nas escolas municipais ocorrem no formato híbrido, por força da Resolução SME n 001/2021'
